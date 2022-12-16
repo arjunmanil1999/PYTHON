155f2bfed448de74df0b4e0c854720b39ac5a146
arjunmanil1999 <116347345+arjunmanil1999@users.noreply.github.com>
2022-12-16 10:34:51 +0530

    Add files via upload

---
+++ listcomprehension.py
@@ -0,0 +1,3 @@
+list=[-10,20,35,-67,70]
+re=[num for num in list if num>=0]
+print(re)
---
+++ overinstead.py
@@ -0,0 +1,12 @@
+n=[]
+s=int(input("enter a limit:"))
+print(f"enter {s} values")
+for i in range(0,s):
+   n.append(int(input()))
+print("\nThe list after assigning:\n")
+for i in range(0,len(n)):
+    if n[i]>=100:
+        print("over")
+else:
+    print(n[i])
+
